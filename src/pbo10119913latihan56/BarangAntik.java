b7a072e72b631dc3a91798b8de2ed5d4261cc293
Dilan <ramadhanrizqi21@gmail.com>
2020-11-22 08:41:59 +0700

    Latihan 56

---
+++ src/pbo10119913latihan56/BarangAntik.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10119913latihan56;
+
+/**
+ * Nama :Muhammad Alvin Rizqi Ramdahn
+ * Kelas : IF10K
+ * NIM : 10119913
+ * @author Lenovo
+ */
+public class BarangAntik {
+    private final int umur;
+
+    public BarangAntik(int umur){
+        this.umur = umur;
+    }
+
+    public void tampilUmur(){
+        System.out.println("Umur barang antik ini adalah : " + this.umur + " tahun");
+    }
+}
---
+++ src/pbo10119913latihan56/PBO10119913Latihan56.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10119913latihan56;
+
+/**
+ * Nama :Muhammad Alvin Rizqi Ramdahn
+ * Kelas : IF10K
+ * NIM : 10119913
+ * @author Lenovo
+ */
+public class PBO10119913Latihan56 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Radio radio1 = new Radio(234);
+        radio1.setName("Radio AM");
+        System.out.println("Nama barang antik : " + radio1.getName());
+        radio1.tampilUmur();
+    }
+    
+}
